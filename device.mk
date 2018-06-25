54e771b21e5560551af20b8f92b99f24257b10e7
Mathieu Chartier <mathieuc@google.com>
2018-06-25 04:07:02 +0200

    rolex: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3

--- device.mk
+++ device.mk
@@ -475,5 +475,8 @@ PRODUCT_PACKAGES += \
 # Reduce system image size by limiting java debug info.
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 
+# Speed profile services and wifi-service to reduce RAM and storage.
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Inherit proprietary files
 $(call inherit-product-if-exists, vendor/xiaomi/rolex/rolex-vendor.mk)
