17483a41b22879127a9a07c4ce7be7e7f54e749b
Arne Coucheron <arco68@gmail.com>
2020-12-03 13:03:50 +0100

    rolex: Use in-process APK for network stack and tethering
    Taken from common go configuration, and the purpose is
    to save some memory.
    
    Change-Id: I32222daf8fa2be1f41a8177b82fa6f2f94e002e7

--- device.mk
+++ device.mk
@@ -482,5 +482,9 @@ PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 # for gms modules.
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 
+# Do not spin up a separate process for the network stack, use an in-process APK.
+PRODUCT_PACKAGES += InProcessNetworkStack
+PRODUCT_PACKAGES += com.android.tethering.inprocess
+
 # Inherit proprietary files
 $(call inherit-product-if-exists, vendor/xiaomi/rolex/rolex-vendor.mk)
