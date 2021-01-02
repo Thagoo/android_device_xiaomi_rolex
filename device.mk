525d89892c03024686e1539b7b376f5140493ade
Thagoo <lohitgowda56@gmail.com>
2021-01-02 09:24:31 +0000

    rolex:seccomp_policy: Import vendor.qti.hardware.dsp.policy
     * dspservice: Could not read base policy file '/vendor/etc/seccomp_policy/vendor.qti.hardware.dsp.policy'
    
    Signed-off-by: Thagoo <lohitgowda56@gmail.com>

--- device.mk
+++ device.mk
@@ -363,7 +363,8 @@ PRODUCT_PACKAGES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/vendor.qti.hardware.dsp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/vendor.qti.hardware.dsp.policy
 
 # Sensors
 PRODUCT_PACKAGES += \
