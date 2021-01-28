ab3c020eadd089e6e6e1d82b4f1104eb05bd79f6
Thagoo <lohitgowda56@gmail.com>
2021-01-28 23:16:44 +0530

    rolex: Build qti healthd HALs

--- device.mk
+++ device.mk
@@ -223,7 +223,7 @@ PRODUCT_PACKAGES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl:64 \
+    android.hardware.health@2.1-impl-qti:64 \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
