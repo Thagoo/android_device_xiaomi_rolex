1502d095a8527b22f21e3ec9324b448e93b3c0e7
Thagoo <lohitgowda56@gmail.com>
2021-02-13 10:51:16 +0530

    rolex: PixelExperience Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rolex.mk
+    $(LOCAL_DIR)/aosp_rolex.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_rolex-user \
-    lineage_rolex-userdebug \
-    lineage_rolex-eng
+    aosp_rolex-user \
+    aosp_rolex-userdebug \
+    aosp_rolex-eng
--- aosp_rolex.mk
+++ aosp_rolex.mk
@@ -21,8 +21,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common Pixel Experience stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
+TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Inherit from rolex device
@@ -32,7 +33,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4A
 PRODUCT_DEVICE := rolex
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_rolex
+PRODUCT_NAME := aosp_rolex
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
