8b77fcd2ef5ea4457d8f65c00530abf45ed4b78f
Thagoo <lohitgowda56@gmail.com>
2021-03-09 17:21:42 +0000

    [squash] Revert "rolex: Switch to TimeKeep"
    This reverts commit 95d7c2f4aa57192e42d6478db12ba7d4cbc5c74f.
    
    Revert "rolex: sepolicy: Allow timekeep to read since_epoch"
    
    This reverts commit 579fd4cb9ac835b7dcafc0e8e0a3221bf80ed86f.
    
    Revert "rolex: sepolicy: Add time_data_file type"
    
    This reverts commit 2663e7b7a9362e612012f338412c3fe94efb6ec0.
    
    Revert "rolex: Address timekeep denial"
    
    This reverts commit 7143bdeb8264906e2df49470213f345fd3577aa5.
    
    Revert "rolex: sepolicy: Resolve remaining TimeKeep denials"
    
    This reverts commit 944f0729d5823af8871eac4a7c0225dc88ce298d.
    
    Revert "rolex: Drop time services"
    
    This reverts commit 89afd46923735f7525a32046fc001c59801e690c.
    
    rolex: Remove timekeep dependecies

--- device.mk
+++ device.mk
@@ -409,11 +409,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
-# TimeKeep
-PRODUCT_PACKAGES += \
-    timekeep \
-    TimeKeep
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
