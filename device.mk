d80c80c1d82b78ce747a77aef23cb3e47def17e0
zer0def <zer0def@zer0def.0>
2021-04-21 08:49:23 +0200

    Add Android 11 camera API

--- device.mk
+++ device.mk
@@ -146,7 +146,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.2 \
     android.hardware.camera.device@3.3 \
     android.hardware.camera.device@3.4 \
+    android.hardware.camera.device@3.5 \
     android.hardware.camera.provider@2.5 \
+    android.hardware.camera.provider@2.6 \
     vendor.qti.hardware.camera.device@1.0
 
 # Charging images
